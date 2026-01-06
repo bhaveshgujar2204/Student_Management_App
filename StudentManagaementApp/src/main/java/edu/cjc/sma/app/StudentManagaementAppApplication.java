abf9d84aededf99328284381f062255736c07f96
Bhavesh Gujar <bhaveshgujar01@gmail.com>
2026-01-06 19:13:16 +0530

    first day commit project

---
+++ StudentManagaementApp/src/main/java/edu/cjc/sma/app/StudentManagaementAppApplication.java
@@ -0,0 +1,13 @@
+package edu.cjc.sma.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagaementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagaementAppApplication.class, args);
+	}
+
+}
